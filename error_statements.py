b377bf484169ffaf1e04e1f9af3b76a80499af6d
Ddhruv-IOT <ddhruvarora2@gmail.com>
2022-12-09 23:29:28 +0530

    file to provide human readable errors

---
+++ error_statements.py
@@ -0,0 +1,37 @@
+no_usb = """It's likely that the tracker is not yet connected!!
+
+ ************************************
+ Did you forget to connect tracker ??
+ Check the connections once.
+ ************************************
+
+---------------- FULL ERROR ----------------
+
+"""
+
+
+serial_err = """It's likely that you removed the tracker and reconnected it!!
+
+ ************************************
+ Disconnect the tracker and reconnect
+ it and start the script again.
+ Kindly avoid removing the tracker
+ when at use.
+ ************************************
+
+---------------- FULL ERROR ----------------
+
+"""
+
+
+unknown_err = """We don't know it yet, if you know what happened, kindly share wit us.
+
+ ***************************************
+ Email the steps to reproduce the error
+ and report it to:
+     ddhruvarora2@gmail.com
+ ***************************************
+
+---------------- FULL ERROR ----------------
+
+"""
